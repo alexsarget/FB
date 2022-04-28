82554c4af5eef097b41c2b8bb557fd4225be5e90
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-04-28 13:14:22 -0500

    5. Agregando el codigo FizzBuzz en Index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Reader=require("./lib/utils/Reader.js")
 const ExplorerService=require("./lib/services/ExplorerServices.js")
+const FizzBuzzService=require("./lib/services/FizzBuzzService")
 
 /// Part 1 Read json file ===========================
 //const rawdata = fs.readFileSync("explorers.json");
@@ -11,12 +12,16 @@ const explorers=Reader.readJsonFile("explorers.json")
 
 //Parte 1 Explorer Service 
 const explorerInNode=ExplorerService.filterByMission(explorers, "node")
-console.log(explorerInNode)
+//console.log(explorerInNode)
 
 //parte 2 Explorer Service
 const explorerInNodeAmount=ExplorerService.getAmountOfExplorersByMission(explorers, "node")
-console.log("Hay "+explorerInNodeAmount+" explorers que estan la mision node")
+//console.log("Hay "+explorerInNodeAmount+" explorers que estan la mision node")
 
 //Parte 3 Explorer Service
 const explorerByUser=ExplorerService.getExplorersUsernamesByMission(explorers, "node")
-console.log(explorerByUser)
+//console.log(explorerByUser)
+
+//Parte 4 FizzBuzz service
+const FizzBuzz= explorers.map((explorer) => FizzBuzzService.applyValidationInExplorer(explorer));
+console.log(FizzBuzz)
